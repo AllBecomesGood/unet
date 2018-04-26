e3314243aaf61d24afcebb931e35e1a85192fe8d
AllBecomesGood <paschnell@dundee.ac.uk>
2018-04-26 19:05:32 +0100

    deleted some comments and stuff

--- data.py
+++ data.py
@@ -2,140 +2,9 @@ from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_a
 import numpy as np
 import os
 import glob
-#from sklearn.preprocessing import StandardScaler 
 from numpy import mean, std
 import nibabel
 from scipy.ndimage import rotate
-#import cv2
-#from libtiff import TIFF
-
-'''
-class myAugmentation(object):
-
-	"""
-	A class used to augment image
-	Firstly, read train image and label seperately, and then merge them together for the next process
-	Secondly, use keras preprocessing to augmentate image
-	Finally, seperate augmentated image apart into train image and label
-	"""
-
-	def __init__(self, train_path="train", label_path="label", merge_path="merge", aug_merge_path="aug_merge", aug_train_path="aug_train", aug_label_path="aug_label", img_type="tif"):
-
-		"""
-		Using glob to get all .img_type from path
-		"""
-
-		self.train_imgs = glob.glob(train_path+"/*."+img_type)
-		self.label_imgs = glob.glob(label_path+"/*."+img_type)
-		self.train_path = train_path
-		self.label_path = label_path
-		self.merge_path = merge_path
-		self.img_type = img_type
-		self.aug_merge_path = aug_merge_path
-		self.aug_train_path = aug_train_path
-		self.aug_label_path = aug_label_path
-		self.slices = len(self.train_imgs)
-		self.datagen = ImageDataGenerator(
-							        rotation_range=0.2,
-							        width_shift_range=0.05,
-							        height_shift_range=0.05,
-							        shear_range=0.05,
-							        zoom_range=0.05,
-							        horizontal_flip=True,
-							        fill_mode='nearest')
-
-	def Augmentation(self):
-
-		"""
-		Start augmentation.....
-		"""
-		trains = self.train_imgs
-		labels = self.label_imgs
-		path_train = self.train_path
-		path_label = self.label_path
-		path_merge = self.merge_path
-		imgtype = self.img_type
-		path_aug_merge = self.aug_merge_path
-		if len(trains) != len(labels) or len(trains) == 0 or len(trains) == 0:
-			print("trains can't match labels")
-			return 0
-		for i in range(len(trains)):
-			img_t = load_img(path_train+"/"+str(i)+"."+imgtype)
-			img_l = load_img(path_label+"/"+str(i)+"."+imgtype)
-			x_t = img_to_array(img_t)
-			x_l = img_to_array(img_l)
-			x_t[:,:,2] = x_l[:,:,0]
-			img_tmp = array_to_img(x_t)
-			img_tmp.save(path_merge+"/"+str(i)+"."+imgtype)
-			img = x_t
-			img = img.reshape((1,) + img.shape)
-			savedir = path_aug_merge + "/" + str(i)
-			if not os.path.lexists(savedir):
-				os.mkdir(savedir)
-			self.doAugmentate(img, savedir, str(i))
-
-
-	def doAugmentate(self, img, save_to_dir, save_prefix, batch_size=1, save_format='tif', imgnum=30):
-
-		"""
-		augmentate one image
-		"""
-		datagen = self.datagen
-		i = 0
-		for batch in datagen.flow(img,
-                          batch_size=batch_size,
-                          save_to_dir=save_to_dir,
-                          save_prefix=save_prefix,
-                          save_format=save_format):
-		    i += 1
-		    if i > imgnum:
-		        break
-
-	def splitMerge(self):
-
-		"""
-		split merged image apart
-		"""
-		path_merge = self.aug_merge_path
-		path_train = self.aug_train_path
-		path_label = self.aug_label_path
-		for i in range(self.slices): #slices = the number of training imgs read in
-			path = path_merge + "/" + str(i)
-			train_imgs = glob.glob(path+"/*."+self.img_type)
-			savedir = path_train + "/" + str(i)
-			if not os.path.lexists(savedir):
-				os.mkdir(savedir)
-			savedir = path_label + "/" + str(i)
-			if not os.path.lexists(savedir):
-				os.mkdir(savedir)
-			for imgname in train_imgs:
-				midname = imgname[imgname.rindex("/")+1:imgname.rindex("."+self.img_type)]
-				img = cv2.imread(imgname)
-				img_train = img[:,:,2]#cv2 read image rgb->bgr
-				img_label = img[:,:,0]
-				cv2.imwrite(path_train+"/"+str(i)+"/"+midname+"_train"+"."+self.img_type,img_train)
-				cv2.imwrite(path_label+"/"+str(i)+"/"+midname+"_label"+"."+self.img_type,img_label)
-
-	def splitTransform(self):
-
-		"""
-		split perspective transform images
-		"""
-		#path_merge = "transform"
-		#path_train = "transform/data/"
-		#path_label = "transform/label/"
-		path_merge = "deform/deform_norm2"
-		path_train = "deform/train/"
-		path_label = "deform/label/"
-		train_imgs = glob.glob(path_merge+"/*."+self.img_type)
-		for imgname in train_imgs:
-			midname = imgname[imgname.rindex("/")+1:imgname.rindex("."+self.img_type)]
-			img = cv2.imread(imgname)
-			img_train = img[:,:,2]#cv2 read image rgb->bgr
-			img_label = img[:,:,0]
-			cv2.imwrite(path_train+midname+"."+self.img_type,img_train)
-			cv2.imwrite(path_label+midname+"."+self.img_type,img_label)
-'''
 
 
 class dataProcess(object):
@@ -154,70 +23,7 @@ class dataProcess(object):
 		self.img_type = img_type
 		self.test_path = test_path
 		self.npy_path = npy_path
-	'''
-	def create_train_data(self):
-		"""
-		Load training images and mask(label) images.
-		Turn them into numpy arrays and save them in a list.
-		Save them into .npy files.
-		"""
-		print('-'*30)
-		print("Entering function: def create_train_data(self):")
-		print('Loading training images and masks...')
 
-		i = 0
-		# Load the 30 .tif images.
-		imgs = glob.glob(self.train_path+"/*."+self.img_type)
-		print("# of imgs found: " + str(len(imgs)))
-		# Create empty placeholder numpy array. (dim here: 30,512,512,1)
-		imgdatas = np.ndarray((len(imgs),self.out_rows,self.out_cols,1), dtype=np.uint8)
-		imglabels = np.ndarray((len(imgs),self.out_rows,self.out_cols,1), dtype=np.uint8)
-
-		# loops thru imgs
-		for imgname in imgs:
-			midname = imgname[imgname.rindex("/")+1:]
-
-			# load_img, img_to_array from keras
-			img = load_img(self.train_path + "/" + midname,grayscale = True)
-			label = load_img(self.label_path + "/" + midname,grayscale = True)
-			img = img_to_array(img)
-			label = img_to_array(label)
-			
-			# pad 0's because Conv2D cannot be allowed to get .5 values, as the upconv will not be same dim then
-			img = np.pad(img, ((22,22),(0,0),(0,0)), 'constant')
-			label = np.pad(label, ((22,22),(0,0),(0,0)), 'constant')
-
-			# Zero mean and unit standard deviatio
-			#sc = StandardScaler()
-			#img = sc.fit_transform(img)
-	        #imgs_test = sc.transform(imgs_test)
-
-			#print("label shape: ")
-			#print(label.shape)
-			#label_inverted = 1 - label #didnt change anything. model predicts all black anyway
-
-			# turned the img and label into numpy array of size (512,512,1)
-			# populate list with img array after img array
-			print(img.shape * 30)
-			imgdatas[i] = img
-			imglabels[i] = label#label_inverted
-			i += 1
-			# Can save created numpy arrays as imgs to see whether they are fine. Looks fine so far.			
-			#img3 = array_to_img(img)
-			#img3.save("./tests/%d.jpg" % (i))
-			#img4 = array_to_img(label)
-			#img4.save("./tests/l%d.jpg" % (i))
-
-		print('Loaded {0}/{1} images.'.format(i, len(imgs)))
-
-		np.save(self.npy_path + '/imgs_train.npy', imgdatas)
-		np.save(self.npy_path + '/imgs_mask_train.npy', imglabels)
-		print('Saving to .npy files done.')
-
-		print("Finished function: def create_train_data(self):")
-		print('-'*30)
-		#return sc
-	'''
 
 	def create_train_data_nii(self):
 		"""
@@ -460,36 +266,6 @@ class dataProcess(object):
 		print("Finished function: def create_test_data_nii(self):")
 		print('-'*30)
 	
-	'''
-	def create_test_data(self):
-		print('-'*30)
-		print("Entering function: def create_test_data(self):")
-		print('Creating test images...')
-
-		i = 0
-		imgs = glob.glob(self.test_path+"/*."+self.img_type)
-		print("# of images found: " + str(len(imgs)))
-		# preallocate array.
-		imgdatas = np.ndarray((len(imgs),self.out_rows,self.out_cols,1), dtype=np.uint8)
-
-		for imgname in imgs:
-			midname = imgname[imgname.rindex("/")+1:]
-			img = load_img(self.test_path + "/" + midname,grayscale = True)
-			img = img_to_array(img)
-			img = np.pad(img, ((22,22),(0,0),(0,0)), 'constant')
-			#img = sc.transform(img)
-			imgdatas[i] = img
-			i += 1
-			#img2 = array_to_img(img)
-			#img2.save("./tests/%d.jpg" % (i))
-
-		print('loading done')
-		np.save(self.npy_path + '/imgs_test.npy', imgdatas)
-		print('Saving to imgs_test.npy files done.')
-
-		print("Finished function: def create_test_data(self):")
-		print('-'*30)
-	'''
 
 	def load_train_data(self):
 		print('-'*30)
@@ -535,10 +311,6 @@ class dataProcess(object):
 
 if __name__ == "__main__":
 
-	#aug = myAugmentation()
-	#aug.Augmentation()
-	#aug.splitMerge()
-	#aug.splitTransform()
 	print("\n")
 	print('='*30)
 	dim1 = 256
